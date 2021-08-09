6580f1e9ae44b280808cab4d4ecbd5e5f9537f65
MBissett29 <mbissett@hotmail.com>
2021-08-09 07:02:29 +0000

    decide a comp choice through a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,31 @@
 /**
  * Player Choice Function
+ * Display Player Choice Function
  */
+
 
 /**
- * Computer Choice Function
+ * Generates a computer choice by assigning it a rondom number
  */
 
-/**
- * Outcome Functions
- */
+function decideCompChoice {
 
+    let compChoice = Math.floor(Math.random() *5) +1
+    
+    if (compChoice === 1) {
+       return "rock";
+    } else if (compChoice === 2) { 
+       return "paper";
+    } else if (compChoice === 3) {
+    return "scissors";
+    } else if (compChoice === 4) {
+    return "lizard";
+    } else if (compChoice === 5) {
+    return "spock";
+    } else {
+        return "error";
+    }
+}
 /**
  * Show Results Function
  */
